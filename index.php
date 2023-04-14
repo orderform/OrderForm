706e99945b8ac124028be7c3d280028f813c068e
orderform <130498477+orderform@users.noreply.github.com>
2023-04-14 12:01:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 // Replace with your Firebase project's API key and database URL
 $apiKey = "AIzaSyDnOjfqFNLlsNNu-Ot5MUvDpoSnlCc0gPc";
 $databaseUrl = "https://order-form-2e781-default-rtdb.firebaseio.com";
+$firebaseEndpoint = $databaseUrl . "/orders.json?auth=" . $apiKey;
 
 // Data to be sent to Firebase
 $data = array(
@@ -16,9 +17,6 @@ $data = array(
 
 // Convert data to JSON format
 $dataJson = json_encode($data);
-
-// Firebase Realtime Database endpoint
-$firebaseEndpoint = $databaseUrl . "/orders.json?auth=" . $apiKey;
 
 // Initialize cURL session
 $ch = curl_init();
@@ -39,10 +37,12 @@ if (curl_errno($ch)) {
     echo 'Order placed successfully';
     
     // Send email to owner
-    $to = "mohdabaans@gmail.com";
+    $to = "deliteapparels@gmail.com";
     $subject = "New Order Placed";
     $message = "A new order has been placed:\n\nName: " . $data["name"] . "\nEmail: " . $data["email"] . "\nPhone: " . $data["phone"] . "\nAddress: " . $data["address"] . "\nTransport: " . $data["transport"] . "\nPayment: " . $data["payment"] . "\nDelivery Address: " . $data["delivery"];
-    $headers = "From: noreply.deliteorderform.com";
+    $headers = "From: orderbot@notifier.com\r\n";
+    $headers .= "Reply-To: ".$_POST["email"]."\r\n";
+    $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
     mail($to, $subject, $message, $headers);
 }
 
